6819f77e4c77436cb2416414aba8653cf942b7e8
SAMARESH-PRADHAN <pradhansamaresh2002@gmail.com>
2025-05-04 12:57:23 +0530

    Moved DB credentials to .env file

--- Backend/dbconfig.py
+++ Backend/dbconfig.py
@@ -2,11 +2,14 @@ import os
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.pool import NullPool
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
 
 POSTGRES_URL="localhost"
 POSTGRES_USER="postgres"
-POSTGRES_PASSWORD="Orsac%40123"
-POSTGRES_DB="weather_data"
+POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB=os.getenv("POSTGRES_DB")
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PASSWORD,url=POSTGRES_URL,db=POSTGRES_DB)
 app.config['SQLALCHEMY_DATABASE_URI']=DB_URL
 app.config['SQLALCHEMY_Track_MODIFICATIONS']=False
